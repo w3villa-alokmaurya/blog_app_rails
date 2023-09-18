beb2941201212239eecba85c9c2dde8c745c9659
Alok Maurya <alok.maurya@w3villa.com>
2023-09-18 18:49:55 +0530

    Botstrap added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_15_100414) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_18_124623) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -66,6 +66,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_15_100414) do
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "confirmed_at"
+    t.string "password_digest"
+    t.string "unconfirmed_email"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
